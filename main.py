dcb76b91738865b8938bdee8930d905766589ccc
hari3747om-bit <hari3747om@gmail.com>
2025-11-26 20:25:28 +0530

    Created the main.py file

---
+++ main.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+arr = np.array([1,2,3,4,5])
+
+arr_2d = np.array([[1,2,3],
+                   [4,5,6],
+                   [7,8,9]])
+
+arr_3d = np.array([[[1,2],[2,3],[3,4]],
+                   [[4,5],[5,6],[6,7]],
+                   [[7,8],[8,9],[9,0]]])
+
+
+
+print(arr)
+print(arr_2d)
+print(arr_3d)
